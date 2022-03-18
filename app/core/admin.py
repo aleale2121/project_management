7882cf5c1e11c896045926ae054ddb3636c1c6d8
aleale2121 <alefewyimer4@gmail.com>
2022-03-18 19:42:39 +0300

    add ed student and staff registrations

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+from core import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['username', 'email']
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login',)})
+
+    )
+    add_fieldsets = (
+        (None, {'classes': ('wide',),
+                'fields': ('username', 'password1', 'password2', 'email')}),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Staff)
+admin.site.register(models.Student)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.conf import settings
+from django.dispatch import receiver
 
-# Create your models here.
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+
+class Batch(models.Model):
+    name = models.CharField(max_length=15, unique=True, primary_key=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        """Creates and save a new user"""
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        user = self.model(username=username, email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_field):
+        """Creates and saves a new superuser"""
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=25, unique=True)
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+        verbose_name='email',
+    )
+    name = models.CharField(max_length=255, default='')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ['email', ]
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, related_name="students", on_delete=models.CASCADE)
+    batch = models.ForeignKey(Batch, related_name="students", on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=15, blank=True)
+    last_name = models.CharField(max_length=15, blank=True)
+
+
+class Staff(models.Model):
+    user = models.OneToOneField(User, related_name="staff", on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=15, blank=True)
+    last_name = models.CharField(max_length=15, blank=True)

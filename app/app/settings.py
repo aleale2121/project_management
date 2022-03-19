a4ca3f5013305629a0c15a808a846c4b6c9d0073
aleale2121 <alefewyimer4@gmail.com>
2022-03-19 16:23:59 +0300

    added cors settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -147,6 +147,10 @@ MEDIA_URL = '/media/'
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

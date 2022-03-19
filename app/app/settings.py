e1fa633a4a4eef5e7a4f0f3029893a6d887dca74
aleale2121 <alefewyimer4@gmail.com>
2022-03-19 15:08:30 +0300

    fix db url

--- app/app/settings.py
+++ app/app/settings.py
@@ -98,9 +98,9 @@ DATABASES = {
     }
 }
 # Heroku: Update database configuration from $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 

2a07928cca047c37caa7a943e9da1b4e78c92328
aleale2121 <alefewyimer4@gmail.com>
2022-03-21 20:58:59 +0300

    add heroku to cors handler

--- app/app/settings.py
+++ app/app/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = "/media/"
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000","https://sfpm.herokuapp.com"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

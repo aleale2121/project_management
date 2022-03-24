fe48a133672050af17e5105240b73d5099d6d1ca
Yuideg <misganewendeg879@gmail.com>
2022-03-24 14:42:21 +0300

    error handling

--- app/util/util.py
+++ app/util/util.py
@@ -1,22 +1,14 @@
-import pathlib
-
-import django.urls
-import users
 from app.constants.constants import ErrorCode, ErrorDetail, ErrorMessage
-from app.users.views import LogoutView
-from app.util.util import ErrorResponse
-from constant import *
 from django.http import JsonResponse
 
 
-def ErrorResponse(code int,model_info,path=""):
-    ErrorResponse={
+
+def ErrorResponse(code=400 ,model_info="",path=""):
+    ErrRes={
         "status_code" :code,
         "error_code"  : ErrorCode[code],
-        "message"    : model_info+ErrorMessage[code],
+        "message"    : ErrorMessage[code],
         "detail"     :ErrorDetail[code],
         "path"       :path     
     }
-    return JsonResponse(ErrorResponse)
-    
-
+    return JsonResponse(ErrRes)

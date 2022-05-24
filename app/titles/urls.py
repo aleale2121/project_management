19fa5366d876de8522812e378549feff8768f52f
aleale2121 <alefewyimer4@gmail.com>
2022-05-24 21:55:34 +0300

    fix titles

--- app/titles/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from .views import TitleViewSet
-
-router = routers.SimpleRouter()
-router.register("titles", TitleViewSet, basename="titles")
-urlpatterns = router.urls
-
